2b336b6e3ef781430368a5300e59c01ad55bcb10
Samuel <scuba.elks-7k@icloud.com>
2025-03-25 14:36:36 +0100

    Create excuse_generator.py

---
+++ excuse_generator.py
@@ -0,0 +1,11 @@
+import random
+
+excuses = [
+    "😷 I can't come, I accidentally superglued myself to my chair.",
+    "🐍 My pet snake ate my homework.",
+    "👻 I was kidnapped by aliens, but they dropped me off late.",
+    "🚀 I had to stop a meteor from hitting Earth.",
+    "💤 I overslept... for 3 days."
+]
+
+print("🎭 Your excuse:", random.choice(excuses))
