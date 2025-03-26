1c25b59c1369bd36cee81ec85af29913bc4976f8
Samuel <scuba.elks-7k@icloud.com>
2025-03-26 21:46:31 +0100

    Create existential_crisis.java

---
+++ existential_crisis.java
@@ -0,0 +1,7 @@
+class ExistentialCrisis {
+    public static void main(String[] args) {
+        System.out.println("🤔 What if this program is just running inside a simulation?");
+        System.out.println("😨 What if YOU are part of it?");
+        while (true) {} // It never stops.
+    }
+}
