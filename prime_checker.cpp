fbd0b41ffa44dc8061672e38c710363ce7033dba
Samuel <scuba.elks-7k@icloud.com>
2025-03-28 08:19:58 +0100

    Create prime_checker.cpp

---
+++ prime_checker.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "🔢 Enter a number: ";
+    cin >> num;
+    
+    if (isPrime(num)) {
+        cout << "✅ " << num << " is a prime number.\n";
+    } else {
+        cout << "❌ " << num << " is not a prime number.\n";
+    }
+    
+    return 0;
+}
